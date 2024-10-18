bb298ed37ae8d20005ee1316ebf34269a6acf26d
Naduni299 <naduni8888974@gmail.com>
2024-10-18 13:36:46 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94775546547"
+const SESSION_ID = "PRABATH-MD~cz8SRBQT#L-IRbn7fvixDwJBRdlRDfukKmk45oLyfVE0pcD8Sncg"
 
 
 module.exports = {
